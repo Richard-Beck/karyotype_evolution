8877272f5b9eb82b404013da60f02cc15be59050
= <=>
2023-01-24 17:14:01 -0500

    added script for running batch jobs

---
+++ rscripts/setup_sim.R
@@ -0,0 +1,70 @@
+options(scipen=999)
+gen_lscape <- function(founder,Npeaks){
+  peaks <- list()
+  pk <- founder
+  heights <- c()
+  ht <- 1
+  sigmas <- c()
+  for(i in 1:Npeaks){
+    ids <- sample(1:length(founder),2)
+    pk[ids] <- sapply(pk[ids], function(j) sample(c(abs(0:(j-1)),(j+1):(2*j)),1))
+    peaks <- c(peaks,list(pk))
+    ht <- ht+runif(1,0.1,0.4)
+    heights <- c(heights,ht)
+    sigmas <- c(sigmas,runif(1,3,10))
+  }
+  
+  sapply(1:length(peaks), function(i) paste(c(peaks[[i]],round(heights[i],digits=3),round(sigmas[i],digits=3)),collapse=","))
+}
+
+gen_config <- function(output_dir="output",init_kary=rep(2,10),fitness_landscape_type="gaussian",
+                       fitness_landscape_file="landscapes/landscape.txt",dt=0.1,
+                       p=0.001,Nsteps=3000,init_size=100000){
+  
+  c(paste(c("init_kary", init_kary),collapse=","),
+    paste(c("fitness_landscape_type",fitness_landscape_type),collapse=","),
+    paste(c("fitness_landscape_file", fitness_landscape_file),collapse=","),
+    paste(c("dt",dt),collapse=","),
+    paste(c("p", p),collapse=","),
+    paste(c("Nsteps", Nsteps),collapse=","),
+    paste(c("output_dir", output_dir),collapse=","),
+    paste(c("init_size", init_size),collapse=","))
+}
+
+gen_replicates <- function(i,batchname){
+  sim_name <- paste0(batchname,"_rep_",stringr::str_pad(i,width=3,pad="0"))
+  cpp_source <- "C:/Users/4473331/Documents/projects/008_birthrateLandscape/karyotype_evolution/ABM/bin/Debug/ABM.exe"
+  cpp_lscape_dir <- "C:/Users/4473331/Documents/projects/008_birthrateLandscape/karyotype_evolution/ABM/landscapes/"
+  cpp_config_dir <- "C:/Users/4473331/Documents/projects/008_birthrateLandscape/karyotype_evolution/ABM/config/"
+  
+  fitness_landscape_file <- paste0(cpp_lscape_dir,sim_name,".txt")
+  config_file <- paste0(cpp_config_dir,sim_name,".txt")
+  
+  cpp_output_dir <- paste0("C:/Users/4473331/Documents/projects/008_birthrateLandscape/karyotype_evolution/ABM/output/",sim_name,"/")
+  dir.create(cpp_output_dir)
+  
+  
+  
+  founder <- rep(2,10)
+  Npeaks <- 5
+  Nruns <- 10
+  lscape <- gen_lscape(founder,Npeaks)
+  cfig <- gen_config(fitness_landscape_file = fitness_landscape_file,
+                     output_dir = cpp_output_dir)
+  writeLines(cfig,config_file)
+  writeLines(lscape,fitness_landscape_file)
+  
+  cmd <- paste(cpp_source,config_file)
+  rep(cmd,Nruns)
+  
+}
+
+batchname <- "InitTest"
+Nruns <- 10
+
+cmds <- unlist(lapply(1:Nruns,gen_replicates,batchname=batchname))
+cmdpath <- paste0("C:/Users/4473331/Documents/projects/008_birthrateLandscape/karyotype_evolution/ABM/cmds/",batchname,".bat")
+writeLines(cmds,cmdpath)
+
+
+
